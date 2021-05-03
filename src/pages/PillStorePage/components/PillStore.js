317f7952a42845645f6f7e91d2f1b5eb093245dc
Sirawit <popeye-zz@hotmail.com>
2021-05-03 20:31:58 +0700

    success fix focus on location

--- src/pages/PillStorePage/components/PillStore.js
+++ src/pages/PillStorePage/components/PillStore.js
@@ -3,11 +3,15 @@ import React, { useContext } from "react";
 
 const PillStore = ({ pillStore }) => {
   
-  const { setIsSelect, selectedPillStore, setTempSelected, setCenter, center } = useContext(MapContext);
+  const { tempSelected, setIsSelect, selectedPillStore, setTempSelected, setCenter, center } = useContext(MapContext);
 
   const checkIfSelected = () => {
     // UserSelectPillStore === pillStoreID now? 
     return selectedPillStore.ID === pillStore.ID
+  }
+
+  const checkIsOnFocus = () => {
+    return (tempSelected.ID === pillStore.ID) && !(selectedPillStore.ID === tempSelected.ID) 
   }
 
   return (
@@ -22,25 +26,25 @@ const PillStore = ({ pillStore }) => {
               lng: pillStore.coordinate.lng + 0.000001,
             });
       }}
-      className= {`disabled:opacity-50 ${checkIfSelected() ? "pointer-events-none":" " } group flex flex-row flex-wrap lg:flex-nowrap items-center justify-center bg-white hover:bg-gray-200 focus:bg-blue-500 focus:outline-none outline-none p-5 w-full h-full focus:ring-blue-500 duration-300`}
+      className= {`disabled:opacity-50 ${checkIsOnFocus()? "bg-blue-500 focus:bg-blue-500 hover:bg-blue-500 pointer-events-none " : " bg-white hover:bg-gray-200 "} ${checkIfSelected() ? "pointer-events-none ":" " } group flex flex-row flex-wrap lg:flex-nowrap items-center justify-center  focus:outline-none outline-none p-5 w-full h-full  duration-300`}
       disabled={checkIfSelected()}
     >
       <div className="flex flex-col w-80 mx-2 flex-grow-8">
-        <h2 className="duration-200 text-lg sm:text-xl text-gray-800 group-hover:text-black group-focus:text-white">
+        <h2 className={`${checkIsOnFocus()? "group-focus:text-white text-white group-hover:text-white" : " text-gray-800 group-hover:text-black"}  text-lg sm:text-xl duration-200`}> 
           {pillStore.pharmacy}
         </h2>
-        <h3 className="duration-200 text-sm sm:text-md text-gray-500 line-clamp-3 sm:line-clamp-2 group-hover:text-gray-700 group-focus:text-gray-300">
+        <h3 className={`${checkIsOnFocus()? "group-focus:text-gray-300 text-gray-300 group-hover:text-gray-300" : " text-gray-500 group-hover:text-gray-700"}  text-sm sm:text-md line-clamp-3 sm:line-clamp-2 duration-200`}>
           {pillStore.location}
         </h3>
       </div>
 
       <h3
-        className={`duration-300 text-md sm:text-base ${
-          pillStore.status
-            ? "bg-green-100 text-green-500 group-focus:bg-green-500 group-focus:text-white"
-            : "bg-red-200 text-red-500"
-        } 
-                        p-3 mx-2 mt-3 md:mt-0 w-40 text-center flex-grow`}
+        className={`duration-300 text-md sm:text-base 
+        ${(!checkIsOnFocus() && pillStore.status)? "bg-green-100 text-green-500 ": " "} 
+        ${(!checkIsOnFocus() && !pillStore.status)? "bg-red-200 text-red-500 ": " "}   
+        ${(checkIsOnFocus() && pillStore.status)? "group-focus:bg-green-500 group-focus:text-white bg-green-500 text-white " : " "}  
+        ${(checkIsOnFocus() && !pillStore.status)? "group-focus:bg-red-300 group-focus:text-red-500 bg-red-300 text-red-500 ":" "}                   
+                                                p-3 mx-2 mt-3 md:mt-0 w-40 text-center flex-grow`}
       >
         {pillStore.status ? "สามารถไปรับยาได้" : "ไม่สามารถไปรับยาได้"}
       </h3>
